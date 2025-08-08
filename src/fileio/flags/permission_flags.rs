9d9dfccef827b93547709e4fc9d8ef3eb0d5126d
bkoshik <bakubakosh@gmail.com>
2025-08-08 18:09:24 +0500

    Add permission_flags.rs

---
+++ src/fileio/flags/permission_flags.rs
@@ -0,0 +1,13 @@
+use nix::sys::stat::Mode;
+
+pub const OWNER_READ: Mode  = Mode::S_IRUSR;
+pub const OWNER_WRITE: Mode = Mode::S_IWUSR ;
+pub const OWNER_EXEC: Mode  = Mode::S_IXUSR ;
+
+pub const GROUP_READ: Mode  = Mode::S_IRGRP ;
+pub const GROUP_WRITE: Mode = Mode::S_IWGRP ;
+pub const GROUP_EXEC: Mode  = Mode::S_IXGRP ;
+
+pub const OTHERS_READ: Mode  = Mode::S_IROTH ;
+pub const OTHERS_WRITE: Mode = Mode::S_IWOTH ;
+pub const OTHERS_EXEC: Mode  = Mode::S_IXOTH ;
