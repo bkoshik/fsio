d5b1c2e6c0656505ee719580eb2ed682f568b51f
bkoshik <bakubakosh@gmail.com>
2025-08-11 22:30:12 +0500

    Bugfix truncate

--- src/file/io/truncate.rs
+++ src/file/io/truncate.rs
@@ -10,11 +10,17 @@ use crate::{
     errors::*,
     file::File
 };
+use crate::flags::whence_flags::END_POS;
 
 impl File {
     pub fn truncate(&mut self, len: usize) -> Result<usize> {
         ftruncate(&mut self.file, len as off_t)
             .map_err(|e| Error::Io(IoError::new(ErrorKind::InvalidInput, e)))?;
+
+        if self.tell()? > self.metadata()?.size() {
+            let _ =self.seek(0, END_POS);
+        }
+
         return Ok(self.metadata()?.size());
     }
 }
