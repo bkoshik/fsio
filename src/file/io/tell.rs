2e81391ae1856e46198829fb06f713daa678058b
bkoshik <bakubakosh@gmail.com>
2025-08-12 16:28:03 +0500

    Update test

--- src/file/io/tell.rs
+++ src/file/io/tell.rs
@@ -1,8 +1,3 @@
-use std::io::{
-    Error as IoError,
-    ErrorKind
-};
-use nix::unistd::lseek;
 use crate::{
     errors::*,
     flags::whence_flags::CURRENT_POS,
@@ -12,12 +7,8 @@ use crate::{
 };
 
 impl File {
-    pub fn tell(&self) -> Result<usize> {
-        return Ok(
-            lseek(&self.file, 0, CURRENT_POS)
-               .map_err(
-                   |e| Error::Io(IoError::new(ErrorKind::InvalidData, e))
-               )? as usize
+    pub fn tell(&mut self) -> Result<usize> {
+        return Ok(self.seek(0, CURRENT_POS)? as usize
        );
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,19 @@ pub mod prelude;
 
 #[cfg(test)]
 mod test {
-    use std::time::Instant;
+    static TEST_COUNT: usize = 1_000;
+
+    use std::{
+        cell::RefCell,
+        time::{
+            Duration,
+            Instant
+        }
+    };
     use serde_json::Value;
     use crate::{
         file::{
+            File,
             FileType,
             FileBuilder
         },
@@ -16,129 +25,361 @@ mod test {
         prelude::*,
     };
 
+    fn get_file() -> Result<File, Box<dyn std::error::Error>> {
+        return Ok(
+            FileBuilder::new()
+                .read_write()
+                .create()
+                .truncate()
+                .permissions(0o755)
+                .open("test.txt")?
+        )
+    }
+
+    fn get_average<F>(get_duration: F) -> Result<(Duration, Duration, Duration), Box<dyn std::error::Error>>
+    where
+        F: Fn() -> Result<Duration, Box<dyn std::error::Error>>,
+    {
+        let results: Vec<Result<Duration, _>> = (0..TEST_COUNT)
+            .into_iter()
+            .map(|_| get_duration())
+            .collect();
+
+        let times: Vec<Duration> = results.into_iter().filter_map(Result::ok).collect();
+        let times_sum = times.iter().sum::<Duration>();
+
+        return Ok((
+            times.iter().max().unwrap().to_owned(),
+            times_sum / times.len() as u32,
+            times.iter().min().unwrap().to_owned()
+        ))
+    }
+
     #[test]
-    fn it_works() -> Result<(), Box<dyn std::error::Error>> {
-        let start_time = Instant::now();
+    fn creating_file() -> Result<(), Box<dyn std::error::Error>> {
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            // Get duration
+            let start_time = Instant::now();
+            let _ = get_file()?;
+            return Ok(start_time.elapsed())
+        };
 
-        let mut file = FileBuilder::new()
-            .read_write()
-            .create()
-            .truncate()
-            .permissions(0o755)
-            .open("test.txt")?;
+        let average_duration = get_average(get_duration)?;
+        println!("Creating average: {:?}", average_duration);
 
-        let duration = start_time.elapsed();
-        println!("Working time creating: {:?}", duration);
+        Ok(())
+    }
 
+    #[test]
+    fn reading_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+        let _ = file.borrow_mut().write("Hello, world!")?;
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+            file.seek(0, START_POS)?;
+
+            // Get duration
+            let start_time = Instant::now();
+            let _ = file.read()?;
+            Ok(start_time.elapsed())
+        };
 
         {
-            let start_time = Instant::now();
+            let mut file = file.borrow_mut();
 
-            let _ = file.write("Helloworld!")?;
-            let data = file.read_from_start()?;
-            assert_eq!(data, "Helloworld!");
+            file.seek(0, START_POS)?;
+            let _ = file.write("Hello, world!")?;
+            file.seek(0, START_POS)?;
+            let data = file.read()?;
 
-            let duration = start_time.elapsed();
-            println!("Working time writing/reading from start: {:?}", duration);
+            assert_eq!(data, "Hello, world!");
         }
 
+        let average_duration = get_average(get_duration)?;
+        println!("Reading average: {:?}", average_duration);
+
+        Ok(())
+    }
+
+    #[test]
+    fn reading_from_start_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+        let _ = file.borrow_mut().write("Hello, world!")?;
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+            let _ = file.seek(0, START_POS)?;
+            let _ = file.write("Hello, world!")?;
+
+            let start_time = Instant::now();
+            let _ = file.replace("l", "")?;
+            Ok(start_time.elapsed())
+        };
 
         {
-            let start_time = Instant::now();
+            let mut file = file.borrow_mut();
 
-            let _ = file.insert(5, ", ")?;
+            file.seek(0, START_POS)?;
+            let _ = file.write("Hello, world!")?;
+            let data = file.read_from_start()?;
+
+            assert_eq!(data, "Hello, world!");
+        }
+
+        let average_duration = get_average(get_duration)?;
+        println!("Reading from start (read + seek(0)) average: {:?}", average_duration);
+
+        Ok(())
+    }
+
+    #[test]
+    fn writing_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+
+            let _ = file.truncate(0);
+
+            let start_time = Instant::now();
+            let _ = file.write("Hello, world!")?;
+            Ok(start_time.elapsed())
+        };
+
+        {
+            let mut file = file.borrow_mut();
+            let _ = file.truncate(0);
+
+            let _ = file.write("Hello, world!")?;
+
             let data = file.read_from_start()?;
             assert_eq!(data, "Hello, world!");
-
-            let duration = start_time.elapsed();
-            println!("Working time inserting/reading from start: {:?}", duration);
         }
 
+        let average_duration = get_average(get_duration)?;
+        println!("Writing average: {:?}", average_duration);
+
+        Ok(())
+    }
+
+    #[test]
+    fn inserting_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+
+            let _ = file.truncate(0)?;
+            let _ = file.write("Helloworld!")?;
+
+            let start_time = Instant::now();
+            let _ = file.insert(5, ", ")?;
+            Ok(start_time.elapsed())
+        };
 
         {
-            let start_time = Instant::now();
+            let mut file = file.borrow_mut();
 
-            let _ = file.truncate(5)?;
+            let _ = file.truncate(0);
+            let _ = file.write("Helloworld!")?;
+            let _ = file.insert(5, ", ")?;
             let data = file.read_from_start()?;
-            assert_eq!(data, "Hello");
 
-            let duration = start_time.elapsed();
-            println!("Working time truncating/reading from start: {:?}", duration);
+            assert_eq!(data, "Hello, world!");
         }
 
+        let average_duration = get_average(get_duration)?;
+        println!("Inserting average: {:?}", average_duration);
 
-        {
-            let start_time = Instant::now();
+        Ok(())
+    }
 
-            let cursor_pos = file.tell()?;
-            assert_eq!(cursor_pos, 5);
+    #[test]
+    fn replacing_by_write_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
 
-            let duration = start_time.elapsed();
-            println!("Working time telling: {:?}", duration);
-        }
-
-
-        {
-            let start_time = Instant::now();
-
-            let _ = file.replace("l", "")?;
-            let data = file.read_from_start()?;
-            assert_eq!(data, "Heo");
-
-            let duration = start_time.elapsed();
-            println!("Working time replacing/reading from start: {:?}", duration);
-        }
-
-
-        {
-            let start_time = Instant::now();
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
 
             let _ = file.seek(0, START_POS)?;
-            let _ = file.write("N")?;
-            let data = file.read_from_start()?;
-            assert_eq!(data, "Neo");
+            let _ = file.write("Hello, world!")?;
+            let _ = file.seek(0, START_POS)?;
 
-            let duration = start_time.elapsed();
-            println!("Working time seeking/writing/reading from start: {:?}", duration);
-        }
-
+            let start_time = Instant::now();
+            let _ = file.write("Ohayo")?;
+            Ok(start_time.elapsed())
+        };
 
         {
+            let mut file = file.borrow_mut();
+
+            let _ = file.seek(0, START_POS)?;
+            let _ = file.write("Hello, world!")?;
+            let _ = file.seek(0, START_POS)?;
+
+            let _ = file.write("Ohayo")?;
+            let data = file.read_from_start()?;
+            assert_eq!(data, "Ohayo, world!");
+        }
+
+        let average_duration = get_average(get_duration)?;
+        println!("Replacing by write (Hello, world! -> Ohayo, World!) average: {:?}", average_duration);
+
+        Ok(())
+    }
+
+    #[test]
+    fn seek_test() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+
+            let _ =file.write("Hello, world!")?;
+
             let start_time = Instant::now();
+            file.seek(0, START_POS)?;
+            Ok(start_time.elapsed())
+        };
+
+        {
+            let mut file = file.borrow_mut();
+
+            file.seek(0, START_POS)?;
+            assert_eq!(file.tell()?, 0);
+        }
+
+        let average_duration = get_average(get_duration)?;
+        println!("Seeking average: {:?}", average_duration);
+
+        Ok(())
+    }
+
+    #[test]
+    fn telling_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+            let _ = file.truncate(0);
+            let _ = file.write("Hello, world!")?;
+
+            let start_time = Instant::now();
+            let _ = file.tell()?;
+            Ok(start_time.elapsed())
+        };
+
+        {
+            let mut file = file.borrow_mut();
+
+            assert_eq!(file.tell()?, 0);
+        }
+
+        let average_duration = get_average(get_duration)?;
+        println!("Telling average: {:?}", average_duration);
+
+        Ok(())
+    }
+
+    #[test]
+    fn truncating_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+
+            let _ = file.seek(0, START_POS)?;
+            let _ =file.write("Hello, world!")?;
+
+            let start_time = Instant::now();
+            file.truncate(5)?;
+            Ok(start_time.elapsed())
+        };
+
+        {
+            let mut file = file.borrow_mut();
+
+            let _ = file.seek(0, START_POS)?;
+            let _ =file.write("Hello, world!")?;
+            let _ = file.truncate(5);
+
+            let data = file.read_from_start()?;
+            assert_eq!(data, "Hello");
+        }
+
+        let average_duration = get_average(get_duration)?;
+        println!("Truncating average: {:?}", average_duration);
+
+        Ok(())
+    }
+
+    #[test]
+    fn metadata_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+            let _ = file.seek(0, START_POS)?;
+            file.write("Hello, world!")?;
+
+            let start_time = Instant::now();
+            let metadata = file.metadata()?;
+            let _ = metadata.file_type().unwrap();
+            let _ = metadata.size();
+            let _ = metadata.uid();
+            let _ = metadata.gid();
+            Ok(start_time.elapsed())
+        };
+
+        {
+            let mut file = file.borrow_mut();
+            let _ = file.seek(0, START_POS)?;
+            file.write("Hello, world!")?;
 
             let metadata = file.metadata()?;
-
             assert_eq!(metadata.file_type().unwrap(), FileType::Regular);
-            assert_eq!(metadata.size(), 3);
-
-            assert!(metadata.uid() > 0, "UID should be positive");
-            assert!(metadata.gid() > 0, "GID should be positive");
-
-            let duration = start_time.elapsed();
-            println!("Working time metadata: {:?}", duration);
+            assert_eq!(metadata.size(), 13);
+            assert!(metadata.uid() > 0, "UID Should be positive");
+            assert!(metadata.gid() > 0, "GID Should be positive");
         }
 
+        let average_duration = get_average(get_duration)?;
+        println!("Metadata average: {:?}", average_duration);
 
-        {
-            let start_time = Instant::now();
+        Ok(())
+    }
 
+    #[test]
+    fn eq_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+            file.write("Hello, world!")?;
             let other_file = FileBuilder::new()
                 .read()
                 .permissions(0o755)
                 .open("test.txt")?;
 
-            assert_eq!(other_file, file);
-
-            let duration = start_time.elapsed();
-            println!("Working time eq: {:?}", duration);
-        }
-
-
-        {
             let start_time = Instant::now();
+            assert_eq!(other_file, *file);
+            Ok(start_time.elapsed())
+        };
 
-            let _ = file.truncate(0);
+        let average_duration = get_average(get_duration)?;
+        println!("Eq average: {:?}", average_duration);
 
+        Ok(())
+    }
+
+    #[test]
+    fn json_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = RefCell::new(get_file()?);
+
+        let get_duration = || -> Result<Duration, Box<dyn std::error::Error>> {
+            let mut file = file.borrow_mut();
+            let _ = file.truncate(0)?;
             let json_data =
                 r#"
                 {
@@ -147,13 +388,16 @@ mod test {
                     "yes?": true
                 }
                 "#;
-            let _ = file.write(json_data)?;
-            let _ = file.json::<Value>()?;
+            let _ =file.write(json_data)?;
 
-            let duration = start_time.elapsed();
-            println!("Working time truncating/writing/deserialize: {:?}", duration);
-        }
+            let start_time = Instant::now();
+            file.json::<Value>()?;
+            Ok(start_time.elapsed())
+        };
 
-        return Ok(());
+        let average_duration = get_average(get_duration)?;
+        println!("Json average: {:?}", average_duration);
+
+        Ok(())
     }
 }
