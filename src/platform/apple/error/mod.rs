74f8fe78067bf1b853d00e62adc14f2ae581fcee
bkoshik <bakubakosh@gmail.com>
2025-08-14 14:13:36 +0500

    Add error: set & clear

--- src/platform/apple/error/mod.rs
+++ src/platform/apple/error/mod.rs
@@ -2,5 +2,7 @@ mod types;
 mod display;
 mod from_raw;
 mod last;
+mod set;
+mod clear;
 
 pub use types::*;
---
+++ src/platform/apple/error/set.rs
@@ -0,0 +1,11 @@
+use crate::error::Error;
+
+impl Error {
+    pub fn set(self) {
+        Self::set_raw(self as i32);
+    }
+    
+    pub fn set_raw(err: i32) {
+        unsafe { *libc::__error() = err };
+    }
+}
