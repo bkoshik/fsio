06592eba25814c7f341360170dec4ff1fe2fa25f
bkoshik <bakubakosh@gmail.com>
2025-08-14 17:05:53 +0500

    Add file method: metadata: file_type

---
+++ src/platform/apple/file/fs/file_metadata/file_type.rs
@@ -0,0 +1,12 @@
+use crate::file::{FileMetadata, FileType};
+use crate::flags::FileTypeFlags;
+
+impl FileMetadata {
+    pub fn file_type(&self) -> Option<FileType> {
+        return FileType::from_raw(self.metadata.st_mode as usize)
+    }
+
+    pub fn file_type_flags(&self) -> FileTypeFlags {
+        return FileTypeFlags::from_bits_truncate(self.metadata.st_mode as usize & FileTypeFlags::MaskType.bits())
+    }
+}
--- src/platform/apple/file/types.rs
+++ src/platform/apple/file/types.rs
@@ -1,5 +1,6 @@
-use std::os::fd::OwnedFd;
+use crate::flags::FileTypeFlags;
 use libc::stat;
+use std::os::fd::OwnedFd;
 
 #[derive(Debug)]
 pub struct File {
@@ -8,4 +9,32 @@ pub struct File {
 
 pub struct FileMetadata {
     pub(crate) metadata: stat
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum FileType {
+    NamedPipe,
+    CharacterDevice,
+    Directory,
+    BlockDevice,
+    Regular,
+    Symlink,
+    Socket,
+}
+
+impl FileType {
+    pub fn from_raw(raw_mode: usize) -> Option<FileType> {
+        let bits = raw_mode & FileTypeFlags::MaskType.bits();
+
+        return match FileTypeFlags::from_bits_truncate(bits) {
+            FileTypeFlags::NamedPipe => Some(FileType::NamedPipe),
+            FileTypeFlags::CharacterDevice => Some(FileType::CharacterDevice),
+            FileTypeFlags::Directory => Some(FileType::Directory),
+            FileTypeFlags::BlockDevice => Some(FileType::BlockDevice),
+            FileTypeFlags::Regular => Some(FileType::Regular),
+            FileTypeFlags::Symlink => Some(FileType::Symlink),
+            FileTypeFlags::Socket => Some(FileType::Socket),
+            _ => None
+        }
+    }
 }
