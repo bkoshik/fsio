05cc6a58b49299e358543522dc68804ebfe1d935
bkoshik <bakubakosh@gmail.com>
2025-08-14 15:11:23 +0500

    Add file method: open

--- src/platform/apple/file/fs/open.rs
+++ src/platform/apple/file/fs/open.rs
@@ -1,11 +1,32 @@
-use std::fs::File;
-use std::io::SeekFrom;
-use std::path::Path;
+use std::ffi::CString;
+use std::os::fd::{FromRawFd, OwnedFd};
+use libc::c_int;
 use crate::flags::{OpenFlags, PermissionFlags};
+use crate::error::*;
+use crate::file::File;
 
-pub trait OpenFS<T>: Sized
+pub trait OpenFS<P>: Sized
 where
-    T: AsRef<Path>,
+    P: AsRef<str>,
 {
-    fn open(path: T, open_flags: OpenFlags, perms: PermissionFlags);
+    fn open(path: P, open_flags: OpenFlags, perms: PermissionFlags) -> Result<Self>;
+}
+
+impl<P> OpenFS<P> for File
+where
+    P: AsRef<str>,
+{
+    fn open(path: P, open_flags: OpenFlags, perms: PermissionFlags) -> Result<Self> {
+        let c_path = CString::new(path.as_ref())
+            .map_err(|_| Error::InvalidArgument)?;
+
+        let raw_fd = unsafe {
+            libc::open(c_path.as_ptr(), open_flags.bits() as c_int, perms.bits())
+        };
+        Error::result(raw_fd)?;
+
+        return Ok(Self {
+            file: unsafe { OwnedFd::from_raw_fd(raw_fd) }
+        })
+    }
 }
