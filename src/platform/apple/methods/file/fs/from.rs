770d1ed2042278bd9a0bec35f7c752839425f35c
bkoshik <bakubakosh@gmail.com>
2025-08-15 16:43:24 +0500

    Add file method feature: from

---
+++ src/platform/apple/methods/file/fs/from.rs
@@ -0,0 +1,30 @@
+use std::os::fd::{FromRawFd, IntoRawFd, OwnedFd, RawFd};
+use crate::file::File;
+
+impl From<RawFd> for File {
+    fn from(fd: RawFd) -> Self {
+        return Self {
+            file: fd as u64
+        }
+    }
+}
+
+impl From<File> for RawFd {
+    fn from(file: File) -> Self {
+        return file.file as i32;
+    }
+}
+
+impl From<OwnedFd> for File {
+    fn from(fd: OwnedFd) -> Self {
+        return Self {
+            file: fd.into_raw_fd() as u64
+        }
+    }
+}
+
+impl From<File> for OwnedFd {
+    fn from(file: File) -> Self {
+        return unsafe { Self::from_raw_fd(file.into()) }
+    }
+}
