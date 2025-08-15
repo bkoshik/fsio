5c8e5ab2b46f2e60b52e15c86b3380d5f71b0754
bkoshik <bakubakosh@gmail.com>
2025-08-15 22:23:18 +0500

    Hotfix x86_64 apple

--- src/platform/apple/x86_64/api.rs
+++ src/platform/apple/x86_64/api.rs
@@ -1,19 +1,19 @@
 pub mod file {
-    pub use crate::platform::apple::types::file::file::*;
-    pub use crate::platform::apple::types::file::seek_whence::*;
+    pub use crate::platform::apple::x86_64::types::file::file::*;
+    pub use crate::platform::apple::x86_64::types::file::seek_whence::*;
 
-    pub use crate::platform::apple::types::file::file_metadata::file_metadata::*;
-    pub use crate::platform::apple::types::file::file_metadata::file_type::*;
-    pub use crate::platform::apple::types::file::file_metadata::permissions::*;
+    pub use crate::platform::apple::x86_64::types::file::file_metadata::file_metadata::*;
+    pub use crate::platform::apple::x86_64::types::file::file_metadata::file_type::*;
+    pub use crate::platform::apple::x86_64::types::file::file_metadata::permissions::*;
 }
 
 pub mod error {
-    pub use crate::platform::apple::types::error::error::*;
-    pub use crate::platform::apple::types::error::result::*;
+    pub use crate::platform::apple::x86_64::types::error::error::*;
+    pub use crate::platform::apple::x86_64::types::error::result::*;
 }
 
 pub mod flags {
-    pub use crate::platform::apple::types::flags::file_type_flags::*;
-    pub use crate::platform::apple::types::flags::open_flags::*;
-    pub use crate::platform::apple::types::flags::permission_flags::*;
+    pub use crate::platform::apple::x86_64::types::flags::file_type_flags::*;
+    pub use crate::platform::apple::x86_64::types::flags::open_flags::*;
+    pub use crate::platform::apple::x86_64::types::flags::permission_flags::*;
 }
